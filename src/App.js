d9cb7eec6130509162d0e181ea97467cb2b06229
David Merrick <david@david-merrick.com>
2019-07-04 11:11:57 -0700

    Re-added material bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Typography, AppBar, Toolbar, Grid } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h4" color="inherit">
+              Aion: iCal filtering service
+            </Typography>
+          </Toolbar>
+          <Grid container spacing={24} justify="center"></Grid>
+        </AppBar>
     </div>
   );
 }
